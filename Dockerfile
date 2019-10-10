88ef08af37b5e9fb32cc9ae2a57102916766fdf4
Yuki Yoshida <yshdyk11202000@gmail.com>
2019-10-10 10:42:52 +0900

    DynamoDBから値をとってそのまま返すアプリ

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,10 @@
 FROM python:3.7.4-alpine
 
-# ソースを置くディレクトリを変数として格納                                                  
 ARG project_dir=/web/hello/
 
-# 必要なファイルをローカルからコンテナにコピー
 ADD hello.py $project_dir
 
-# requirements.txtに記載されたパッケージをインストール                         
 WORKDIR $project_dir
-RUN pip install Flask
+RUN pip install Flask aws-xray-sdk boto3
 
-# （コンテナ内で作業する場合）必要なパッケージをインストール
-RUN apk update                  
-RUN apk add zsh vim tmux git tig
-
-#CMD ["python, /web/hello/hello.py"]
 CMD ["python", "/web/hello/hello.py"]
-#RUN ["find", "/web/"]
-
--- hello.py
+++ hello.py
@@ -1,9 +1,50 @@
 from flask import Flask                           
-app = Flask(__name__)                             
+import boto3
+import json
+import traceback
+import sys
+import decimal
+from aws_xray_sdk.core import xray_recorder, patch_all
 
-@app.route('/')                                   
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if abs(o) % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+ddb = boto3.resource('dynamodb')
+table = ddb.Table('appmesh_sample_message')
+app = Flask(__name__)                             
+patch_all()
+
+@app.route('/')
 def hello_world():                                
     return '{"message": "Hello World!"}'
+
+@app.route('/ddb')
+def ddb_message():                                
+    xray_recorder.begin_segment('ddb_message')
+
+    try:
+        response = table.get_item(
+                Key={
+                    'id': 1
+                }
+            )
+        item = response['Item']
+        app.logger.info("----------")
+        app.logger.info(item)
+        app.logger.info("----------")
+        app.logger.info(json.dumps(item, indent=4, cls=DecimalEncoder))
+    except Exception:
+        app.logger.info(traceback.print_exc())
+
+    xray_recorder.end_segment()
+    return item['message']
 
 if __name__ == '__main__':                        
     app.run(host="0.0.0.0", port=5000, debug=True)
